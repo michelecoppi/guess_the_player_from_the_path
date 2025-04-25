96f31cd38a3ab640e74acbe4f3cab81fe0870272
michelecoppi <supergimmy878@gmail.com>
2025-04-25 17:44:12 +0200

    flask and bot running in the same time

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
 from flask import Flask
 import os
-import asyncio
+import threading
 from handlers.start_handler import start
 from config import BOT_TOKEN
 
@@ -11,15 +11,19 @@ flask_app = Flask(__name__)
 def home():
     return "Bot Telegram è online!"
 
-async def run_telegram_bot():
+def run_telegram_bot():
     telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
     telegram_app.add_handler(CommandHandler("start", start))
-    await telegram_app.run_polling()
+    telegram_app.run_polling()
 
-async def main():
-    asyncio.create_task(run_telegram_bot())
-
+def run_flask_app():
     flask_app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)), use_reloader=False)
 
+def main():
+    flask_thread = threading.Thread(target=run_flask_app)
+    flask_thread.start()
+    
+    run_telegram_bot()
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
