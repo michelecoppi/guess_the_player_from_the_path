9b52ca33afee47ea8adf413a5492ef140e81ddf6
michelecoppi <supergimmy878@gmail.com>
2025-04-25 17:26:20 +0200

    add asyncio to run_telegram_bot

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler
 from flask import Flask
 import os
 import threading
+import asyncio
 from handlers.start_handler import start
 from config import BOT_TOKEN
 
@@ -14,9 +15,15 @@ def home():
 
 # Funzione per avviare il bot Telegram
 def run_telegram_bot():
-    telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
-    telegram_app.add_handler(CommandHandler("start", start))
-    telegram_app.run_polling()
+    async def main():
+        telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
+        telegram_app.add_handler(CommandHandler("start", start))
+        await telegram_app.run_polling() 
+
+    loop = asyncio.new_event_loop()  
+    asyncio.set_event_loop(loop)      
+    loop.run_until_complete(main())   
+
 
 if __name__ == '__main__':
     threading.Thread(target=run_telegram_bot).start()
