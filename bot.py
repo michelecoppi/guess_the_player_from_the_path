a3c3f236f52dc47d99454d87ec9855b77641d1f2
michelecoppi <supergimmy878@gmail.com>
2025-04-25 17:34:43 +0200

    create first with asynco in main

--- bot.py
+++ bot.py
@@ -5,22 +5,21 @@ import asyncio
 from handlers.start_handler import start
 from config import BOT_TOKEN
 
-
 flask_app = Flask(__name__)
 
 @flask_app.route('/')
 def home():
     return "Bot Telegram è online!"
 
-
 async def run_telegram_bot():
     telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
     telegram_app.add_handler(CommandHandler("start", start))
     await telegram_app.run_polling()
 
-if __name__ == '__main__':
-    async def start_bot():
-        await run_telegram_bot()
+async def main():
+    asyncio.create_task(run_telegram_bot())
 
-    asyncio.create_task(start_bot())
-    flask_app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
+    flask_app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)), use_reloader=False)
+
+if __name__ == '__main__':
+    asyncio.run(main())
