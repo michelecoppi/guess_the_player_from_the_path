eb9755c9fabffcdbd84846725074af7575aa2673
michelecoppi <supergimmy878@gmail.com>
2025-04-25 17:30:54 +0200

    fix asynco in bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
 from flask import Flask
 import os
-import threading
 import asyncio
 from handlers.start_handler import start
 from config import BOT_TOKEN
@@ -13,19 +12,15 @@ flask_app = Flask(__name__)
 def home():
     return "Bot Telegram è online!"
 
-# Funzione per avviare il bot Telegram
-def run_telegram_bot():
-    async def main():
-        telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
-        telegram_app.add_handler(CommandHandler("start", start))
-        await telegram_app.run_polling() 
 
-    loop = asyncio.new_event_loop()  
-    asyncio.set_event_loop(loop)      
-    loop.run_until_complete(main())   
-
+async def run_telegram_bot():
+    telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
+    telegram_app.add_handler(CommandHandler("start", start))
+    await telegram_app.run_polling()
 
 if __name__ == '__main__':
-    threading.Thread(target=run_telegram_bot).start()
+    async def start_bot():
+        await run_telegram_bot()
 
+    asyncio.create_task(start_bot())
     flask_app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
