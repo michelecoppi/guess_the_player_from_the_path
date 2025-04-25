50f110c2b20fc766270c6799a001f34413c58ff2
michelecoppi <supergimmy878@gmail.com>
2025-04-25 17:20:38 +0200

    modified to keep alive on render the bot

--- bot.py
+++ bot.py
@@ -1,13 +1,24 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
+from flask import Flask
+import os
+import threading
 from handlers.start_handler import start
-from config import BOT_TOKEN 
+from config import BOT_TOKEN
 
 
+flask_app = Flask(__name__)
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
+@flask_app.route('/')
+def home():
+    return "Bot Telegram è online!"
 
+# Funzione per avviare il bot Telegram
+def run_telegram_bot():
+    telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
+    telegram_app.add_handler(CommandHandler("start", start))
+    telegram_app.run_polling()
 
-app.add_handler(CommandHandler("start", start))
+if __name__ == '__main__':
+    threading.Thread(target=run_telegram_bot).start()
 
-
-app.run_polling()
+    flask_app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
