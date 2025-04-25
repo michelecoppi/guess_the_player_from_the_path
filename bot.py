3a6b600e4278b8936ab17f76ac8104e9fe6d4460
michelecoppi <supergimmy878@gmail.com>
2025-04-25 18:24:13 +0200

    add app_initialize

--- bot.py
+++ bot.py
@@ -1,19 +1,18 @@
 from flask import Flask, request
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler
-from handlers.start_handler import start
 from config import BOT_TOKEN, WEBHOOK_URL
+from handlers.start_handler import start
 import asyncio
 import os
 
 flask_app = Flask(__name__)
-
 telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
 telegram_app.add_handler(CommandHandler("start", start))
 
 @flask_app.route("/")
 def index():
-    return "Bot attivo e pronto a ricevere webhook!"
+    return "Bot attivo!"
 
 @flask_app.route("/webhook", methods=["POST"])
 async def webhook():
@@ -22,10 +21,12 @@ async def webhook():
     await telegram_app.process_update(update)
     return "OK", 200
 
-async def set_webhook():
+async def start_bot():
+    await telegram_app.initialize()
     await telegram_app.bot.set_webhook(WEBHOOK_URL)
+    await telegram_app.start()
 
 if __name__ == "__main__":
-    asyncio.run(set_webhook())
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_bot())
     flask_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
-
